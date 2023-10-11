e70c6ed26f7269b38b19468f36c9bb914442c632
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-10-11 18:51:14 +0900

    add omit

---
+++ omit.ts
@@ -0,0 +1,26 @@
+import type { Equal, Expect, NotAny } from './util'
+
+type MyOmit<T, K> = any
+
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+]
+
+type error = MyOmit<Todo, 'description' | 'invalid'>
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+  completed: boolean
+}
+
+interface Expected2 {
+  title: string
+}
