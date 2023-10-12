a90146daad1cbec115e6827a1b5c8a868870fb87
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-10-12 21:30:54 +0900

    add omit answer

--- omit.ts
+++ omit.ts
@@ -1,26 +1,28 @@
-import type { Equal, Expect, NotAny } from './util'
+import type { Equal, Expect, NotAny } from "./util";
 
-type MyOmit<T, K> = any
-
+type MyOmit<T, K extends keyof T> = {
+  [t in keyof T as t extends K ? never : t]: T[t];
+};
 
 type cases = [
-  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
-  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
-]
+  Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
+];
 
-type error = MyOmit<Todo, 'description' | 'invalid'>
+// @ts-expect-error
+type error = MyOmit<Todo, "description" | "invalid">;
 
 interface Todo {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
 }
 
 interface Expected1 {
-  title: string
-  completed: boolean
+  title: string;
+  completed: boolean;
 }
 
 interface Expected2 {
-  title: string
+  title: string;
 }
