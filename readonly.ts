49f0b3fa50dcbae206756e56181acf83c4a08445
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-10-13 17:38:56 +0900

    add readonly

--- readonly.ts
+++ readonly.ts
@@ -1,26 +1,31 @@
-import type { Equal, Expect, NotAny } from './util'
+import type { Equal, Expect, Alike } from "./util";
 
-type MyReadonly<T> = any
-
-interface Todo {
-  title: string
-  description: string
-}
-
-const todo: MyReadonly<Todo> = {
-  title: "Hey",
-  description: "foobar"
-}
+type MyReadonly2<T, K> = any;
 
 type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "description">, Expected>>
+];
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, "title" | "invalid">;
 
 interface Todo1 {
-  title: string
-  description: string
-  completed: boolean
-  meta: {
-    author: string
-  }
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Todo2 {
+  readonly title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Expected {
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
 }
