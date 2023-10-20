cf9743673596043ec0d0a9b1e1cfbfba11a94a89
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-10-20 20:14:18 +0900

    add exclude answer

--- exclude.ts
+++ exclude.ts
@@ -1,6 +1,8 @@
 import type { Equal, Expect, NotAny } from "./util";
 
-type MyExclude<T, U> = any;
+type MyExclude<T, U> = T extends U ? never : T;
+
+type aaaaa = MyExclude<"a" | "b" | "c", "c">;
 
 type cases = [
   Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
