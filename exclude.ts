231f53489ac70a1f3c302ff5fe4e02fcc58071c5
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-10-20 20:07:33 +0900

    add exclude

---
+++ exclude.ts
@@ -0,0 +1,11 @@
+import type { Equal, Expect, NotAny } from "./util";
+
+type MyExclude<T, U> = any;
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
