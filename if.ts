feb9eef3868cae40a301274c3c5fcf066b9aa2d6
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-10-22 09:58:06 +0900

    fix if answer

--- if.ts
+++ if.ts
@@ -1,10 +1,11 @@
 import type { Equal, Expect, NotAny } from "./util";
 
-type If<C, T, F> = C extends true ? T : F;
+type If<C extends boolean, T, F> = C extends true ? T : F;
 
 type cases = [
   Expect<Equal<If<true, "a", "b">, "a">>,
   Expect<Equal<If<false, "a", 2>, 2>>
 ];
 
+// @ts-expect-error
 type error = If<null, "a", "b">;
