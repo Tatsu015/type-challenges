7397bdb9679b28dae7d632e7495298e31367ddab
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-11-01 18:35:09 +0900

    add last of array

---
+++ lastofarray.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect, NotAny, Alike } from "./util";
+
+type Last<T extends any[]> = any;
+
+type cases = [
+  Expect<Equal<Last<[2]>, 2>>,
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>
+];
