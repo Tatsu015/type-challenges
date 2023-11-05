c2769d14cdced9c08f9f4e6a310c9504e2a05929
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-11-05 20:32:29 +0900

    add type lookup

---
+++ typelookup.ts
@@ -0,0 +1,21 @@
+import type { Equal, Expect, NotAny, Alike } from "./util";
+
+type LookUp<U, T> = any;
+
+interface Cat {
+  type: "cat";
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal";
+}
+
+interface Dog {
+  type: "dog";
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer";
+  color: "brown" | "white" | "black";
+}
+
+type Animal = Cat | Dog;
+
+type cases = [
+  Expect<Equal<LookUp<Animal, "dog">, Dog>>,
+  Expect<Equal<LookUp<Animal, "cat">, Cat>>
+];
