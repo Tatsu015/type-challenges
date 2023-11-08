c693600ee147a572f043259e2318e65a953eb49c
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-11-09 08:48:28 +0900

    add replace answer

--- replace.ts
+++ replace.ts
@@ -1,6 +1,14 @@
 import type { Equal, Expect, NotAny, Alike } from "./util";
 
-type Replace<S extends string, From extends string, To extends string> = any;
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer Pre}${From}${infer Suf}`
+  ? `${Pre}${To}${Suf}`
+  : S;
 
 type cases = [
   Expect<Equal<Replace<"foobar", "bar", "foo">, "foofoo">>,
