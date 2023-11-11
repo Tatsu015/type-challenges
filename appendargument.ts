af42e5369c91a86ddb7abdb505ad7de40dc3c4f8
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-11-11 19:03:48 +0900

    add append argment answer

--- appendargument.ts
+++ appendargument.ts
@@ -1,6 +1,8 @@
 import type { Equal, Expect, NotAny, Alike } from "./util";
 
-type AppendArgument<Fn, A> = any;
+type AppendArgument<Fn, A> = Fn extends (...args: infer Arg) => infer Ret
+  ? (...args: [...Arg, A]) => Ret
+  : undefined;
 
 type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
 type Result1 = (a: number, b: string, x: boolean) => number;
