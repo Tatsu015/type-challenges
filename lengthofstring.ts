5206d6d9de734492bd9e2a85492261609b39c7c2
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-11-13 12:24:28 +0900

    add length of string

---
+++ lengthofstring.ts
@@ -0,0 +1,10 @@
+import type { Equal, Expect, NotAny, Alike } from "./util";
+
+type LengthOfString<S extends string> = any;
+
+type cases = [
+  Expect<Equal<LengthOfString<"">, 0>>,
+  Expect<Equal<LengthOfString<"kumiko">, 6>>,
+  Expect<Equal<LengthOfString<"reina">, 5>>,
+  Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
+];
